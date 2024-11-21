c2e3c80c3947c7104e18a2438ee163a24b664a2e
fynn emmanuel junior <fynn.emmanuel.junior@gmail.com>
2024-11-21 15:37:10 +0000

    updated backend

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import {  Body, Controller, Delete, Get, Param, Post, Put,UseGuards} from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/auth.guard';
+import { CategoryService } from './category.service';
+import { categoryDto } from './category.dto';
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+    constructor (private readonly categoryService: CategoryService){}
+
+    @UseGuards(JwtAuthGuard)
+    @Post()
+    async createCategory(@Body() createCategoryDto: categoryDto) {
+        return this.categoryService.create(createCategoryDto);
+    }
+
+}
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,Inject } from '@nestjs/common';
+import { Knex } from 'knex';
+import { categoryDto } from './category.dto';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+    constructor(@Inject('KnexConnection') private readonly knex: Knex) {}
+
+    async create(data: categoryDto) {
+        const {name,description} = data;
+        await this.knex('categories').insert({name, description});
+    }
+}
