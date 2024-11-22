c613fe1566375e890f124e5946d2110e93c3effd
fynn emmanuel junior <fynn.emmanuel.junior@gmail.com>
2024-11-22 20:50:47 +0000

    updated backend

--- src/migrations/20241122204831_add_column_table.ts
+++ src/migrations/20241122204831_add_column_table.ts
@@ -2,10 +2,16 @@ import type { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
+    return knex.schema.table('users',(table) =>{
+        table.string('role').defaultTo('user');
+    })
 
 }
 
 
 export async function down(knex: Knex): Promise<void> {
+    return knex.schema.table('users',(table) => {
+        table.dropColumn('role');
+    })
 }
 
