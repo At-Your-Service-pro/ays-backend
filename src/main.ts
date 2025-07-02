7a0ee42bf4e2d48d4c5172f6ce76ab38276a688f
fynn emmanuel junior <fynn.emmanuel.junior@gmail.com>
2025-07-02 11:56:42 +0000

    updated docker file

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express'; 
+import { Logger } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import * as dotenv from 'dotenv'; 
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // Trust the proxy (Nginx)
+  app.set('trust proxy', true);
+  
   app.use(cookieParser());
   app.enableCors({
-    origin: '*',
+    origin: true,
     credentials: true,
     allowedHeaders: [
       'Access-Control-Allow-Origin',  
@@ -19,7 +24,11 @@ async function bootstrap() {
     methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'], 
 
   });
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3104;
+  await app.listen(port, '0.0.0.0'); // Listen on all interfaces
+  
+  Logger.log(`🚀 Application running on port ${port}`, 'Bootstrap');
+  Logger.log(`🔁 Nginx proxy at http://localhost`, 'Docker');
   console.log(`Server running on post ${process.env.PORT}`); 
 }
 bootstrap();
